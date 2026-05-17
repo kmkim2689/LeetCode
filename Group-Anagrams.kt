f5b59fa8afd9d26781d044946c80cdc4012f34d1
Kimoon Kim <101035437+kmkim2689@users.noreply.github.com>
2026-05-17 21:12:33 +0200

    [49] [Time Beats: 96.99%] [Memory Beats: 90.74%] - LeetPush

--- Group-Anagrams.kt
+++ Group-Anagrams.kt
@@ -2,35 +2,32 @@
 2    fun groupAnagrams(strs: Array<String>): List<List<String>> {
 3        val solution = hashMapOf<String, ArrayList<String>>()
 4        for (str in strs) {
-5            val sorted = str.toList().sorted().joinToString("")
-6            val value = solution[sorted]
-7            if (value == null) {
-8                solution[sorted] = arrayListOf(str)
-9            } else {
-10                value.add(str)
-11            }
-12        }
-13
-14        return solution.map { it.value }
-15    }
-16}
-17
-18/**
-19eat
-20tea
-21tan
-22ate
-23nat
-24bat
-25
-26e - 1
-27a - 1
-28t - 1
-29
-30aa
-31aa
-32bb
-33bb
-34c
-35cc
-36 */
+5            val charArr = str.toCharArray()
+6            charArr.sort()
+7            val sortedStr = String(charArr)
+8            solution.getOrPut(sortedStr, { arrayListOf<String>() }).add(str)
+9        }
+10
+11        return ArrayList(solution.values)
+12    }
+13}
+14
+15/**
+16eat
+17tea
+18tan
+19ate
+20nat
+21bat
+22
+23e - 1
+24a - 1
+25t - 1
+26
+27aa
+28aa
+29bb
+30bb
+31c
+32cc
+33 */
